19d1d55ba35afdd001d5abfa906719889e0d7e2a
Colin McCloskey <colin.mccloskey@gmail.com>
2012-03-23 14:12:42 -0400

    WOSoMP Initial; public.html

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wosomp::Application.config.secret_token = '99bfd229b93ad2440afd68cd0fe8752e7c188fb5f4437ed355b64616885eb96664c96f4a089e08c9adb27c7e261485990faa4acd735e2ecf16c43bfc1352f4ef'
