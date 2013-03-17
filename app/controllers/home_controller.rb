6337b4d528a1b9ad072019df95432c8b28da9199
Colin McCloskey <colin.mccloskey@gmail.com>
2013-03-17 03:39:14 -0400

    lots of changes; front page redesign

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,14 +2,14 @@
 # HomeController -- User landing pages
 
 class HomeController < ApplicationController
-  
+
   before_filter :authenticate_user!, :except=>[:index]
-  
+
   def index
-    flash.now[:info] = "You are wonderful and awesome!"
+    flash.now[:info] = "This is the flash message"
     @current_olympiad = Olympiad.current
     @registerrable_olympiads = Olympiad.registration_open
     @planning_olympiads = Olympiad.planning_open
   end
-  
+
 end
