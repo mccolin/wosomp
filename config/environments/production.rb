ff06c389beea4d1eaf8e915ad9ebce01babc6161
Colin McCloskey <colin.mccloskey@gmail.com>
2013-03-18 01:02:12 -0400

    link admin header to site; active_admin precompile/compile asset fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ Wosomp::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true  #default:false
 
   # Generate digests for assets URLs
   config.assets.digest = true
@@ -47,6 +47,7 @@ Wosomp::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
+  config.assets.precompile += [/^a-z0-9+(css|js)$/]
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -10,7 +10,7 @@ ActiveAdmin.setup do |config|
   # Set the link url for the title. For example, to take
   # users to your main site. Defaults to no link.
   #
-  # config.site_title_link = "/"
+  config.site_title_link = "/"
 
   # Set an optional image to be displayed for the header
   # instead of a string (overrides :site_title)
@@ -57,7 +57,7 @@ ActiveAdmin.setup do |config|
   # within the controller.
   # Suggested: config.authentication_method = :authenticate_admin_user!
   config.authentication_method = :authenticate_active_admin_user!   # ApplicationController
-  
+
   # == Current User
   #
   # Active Admin will associate actions with the current
@@ -81,7 +81,7 @@ ActiveAdmin.setup do |config|
   # Default:
   # config.logout_link_path = :destroy_admin_user_session_path
   config.logout_link_path      = :destroy_user_session_path
-  
+
   # This setting changes the http method used when rendering the
   # link. For example :get, :delete, :put, etc..
   #
@@ -119,7 +119,7 @@ ActiveAdmin.setup do |config|
   # Enable and disable Batch Actions
   #
   config.batch_actions = true
-  
+
 
   # == Controller Filters
   #
@@ -137,7 +137,7 @@ ActiveAdmin.setup do |config|
   #
   # To load a stylesheet:
   #   config.register_stylesheet 'my_stylesheet.css'
-  
+
   # You can provide an options hash for more control, which is passed along to stylesheet_link_tag():
   #   config.register_stylesheet 'my_print_stylesheet.css', :media => :print
   #
