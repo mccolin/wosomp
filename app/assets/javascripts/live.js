055217523cb65e4f8b3015cb3236f1593e826f33
Colin McCloskey <colin.mccloskey@gmail.com>
2013-04-30 03:05:06 -0400

    live page switching starter

--- app/assets/javascripts/live.js
+++ app/assets/javascripts/live.js
@@ -4,6 +4,59 @@
  *
 **/
 
+/** Load a page of the display into the viewport: **/
+function loadPage(href, callbackFn) {
+  jQuery.ajax(href, {
+    type: "GET",
+    url: href,
+    success: function(html, status, xhr){
+      $("#content").html(html);
+      if (callbackFn)
+        callbackFn();
+    },
+    error: function(xhr, status, msg){
+      alert("AJAX load error");
+    }
+  });
+}
+
+/** Trigger a load for the next page in the navigation: **/
+CURRENT_LIVE_TAB = -1;
+function loadNextPage(callbackFn) {
+  var $navMenu = $("#top-nav");
+  var numTabs = $navMenu.find("li").length;
+  var nextTabIdx = CURRENT_LIVE_TAB + 1;
+
+  console.log("Within loadNextPage:");
+  console.log("numTabs: "+numTabs);
+  console.log("CURRENT_LIVE_TAB: "+CURRENT_LIVE_TAB);
+  console.log("nextTabIdx: "+nextTabIdx);
+
+  var $nextNav = $( $navMenu.find("li")[nextTabIdx] );
+
+  console.log("nextNav:");
+  console.log($nextNav);
+
+  var $navLink = $nextNav.find("a").first();
+
+  console.log("navLink:");
+  console.log($navLink);
+
+  CURRENT_LIVE_TAB = nextTabIdx;
+  console.log("CURRENT_LIVE_TAB increased to "+CURRENT_LIVE_TAB);
+
+  loadPage(
+    $navLink.prop("href"),
+    function(){
+      setTimeout('loadNextPage()', 4000);
+    }
+  );
+
+
+
+
+}
+
 
 /** Given the #id of a leaderboard, calculate and display it properly: */
 function updateLeaderBoard(boardId) {
